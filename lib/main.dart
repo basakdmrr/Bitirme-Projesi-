8a49490683dc0d49e6968ef1fc70c2f1e32157d4
yusrasengun4 <132217043+yusrasengun4@users.noreply.github.com>
2026-02-13 23:35:26 +0300

    Refactor main app structure and update title

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
+import 'pages/role_select.dart';
 
-import 'app/app_container.dart';
-import 'screens/splash_screen.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 🔗 Service & Repository init
-  AppContainer.init();
-
-  runApp(const BreathAIApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class BreathAIApp extends StatelessWidget {
-  const BreathAIApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Breath AI',
+      title: 'Hastalık Analiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-        primaryColor: const Color(0xFF0EA5E9),
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Roboto',
+        primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: const RoleSelectScreen(), // 🔥 Artık buradan başlıyor
     );
   }
 }
+
